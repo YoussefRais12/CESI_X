29d358090b6a39ac2538065b3ff1d83c06d58602
Depy y <ssefy150@gmail.com>
2024-05-03 14:29:04 +0200

    test

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,14 @@
+import * as z from "zod"
+
+export const SignupValidation = z.object({
+    name: z
+        .string()
+        .min(2, {message: "Name is too short"}),
+    username: z
+        .string()
+        .min(2, {message: "Username is too short"}),
+    email: z
+        .string()
+        .email(),
+        password: z.string().min(8, {message: "Password must have at least 8 characters"})
+})

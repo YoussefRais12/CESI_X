96579c1980ac0eb10cd4cbc1439fc206d8ce96a1
Arlan KOWO <arlan.kowokonmeneck@viacesi.fr>
2024-05-29 14:41:42 +0200

    Some changes

--- server/routes/restaurantRoute.js
+++ server/routes/restaurantRoute.js
@@ -20,67 +20,67 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.get("/data.registered", async (req, res) => {
-  const { ownerId } = req.params;
-  try {
-    const restaurant = await Restaurant.findById(ownerId);
-    res.json(restaurant);
-  } catch (error) {
-    console.error("An error occured:", error.message);
-    return { error: "Veuillez rééssayer je vous prie" };
-  }
-});
+// router.get("/data.registered", async (req, res) => {
+//   const { ownerId } = req.params;
+//   try {
+//     const restaurant = await Restaurant.findById(ownerId);
+//     res.json(restaurant);
+//   } catch (error) {
+//     console.error("An error occured:", error.message);
+//     return { error: "Veuillez rééssayer je vous prie" };
+//   }
+// });
 
-router.delete("/:dropid", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const restaurant = await Restaurant.findByIdAndDelete(id);
-    if (!restaurant) {
-      console.error("An error occured:", error.message);
-      return res
-        .status(404)
-        .json({ error: "The restaurant hasn't been found" });
-    }
-    res.status(200).json({ message: "Restaurant deleted successfully" });
-  } catch (error) {
-    console.error("An error occured:", error.message);
-    return { error: "Let's try again please:" };
-  }
-});
+// router.delete("/:dropid", async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const restaurant = await Restaurant.findByIdAndDelete(id);
+//     if (!restaurant) {
+//       console.error("An error occured:", error.message);
+//       return res
+//         .status(404)
+//         .json({ error: "The restaurant hasn't been found" });
+//     }
+//     res.status(200).json({ message: "Restaurant deleted successfully" });
+//   } catch (error) {
+//     console.error("An error occured:", error.message);
+//     return { error: "Let's try again please:" };
+//   }
+// });
 
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const restaurant = await Restaurant.findById(id);
-    if (!restaurant) {
-      console.error("An error occured:", error.message);
-      return res
-        .status(404)
-        .json({ error: "The restaurant hasn't been found" });
-    }
-    res.status(200).json(restaurant);
-  } catch (error) {
-    console.error("An error occured:", error.message);
-    return res.status(200).json({ error: "The restaurant hasn't been found" });
-  }
-});
+// router.get("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const restaurant = await Restaurant.findById(id);
+//     if (!restaurant) {
+//       console.error("An error occured:", error.message);
+//       return res
+//         .status(404)
+//         .json({ error: "The restaurant hasn't been found" });
+//     }
+//     res.status(200).json(restaurant);
+//   } catch (error) {
+//     console.error("An error occured:", error.message);
+//     return res.status(200).json({ error: "The restaurant hasn't been found" });
+//   }
+// });
 
-router.get("/:name", async (req, res) => {
-    const { name } = req.params;
-    try {
-      const restaurant = await Restaurant.findOne(name);
-      if (!restaurant) {
-        console.error("An error occured:", error.message);
-        return res
-          .status(404)
-          .json({ error: "The restaurant hasn't been found" });
-      }
-      res.status(200).json(restaurant);
-    } catch (error) {
-      console.error("An error occured:", error.message);
-      return res.status(200).json({ error: "The restaurant hasn't been found" });
-    }
-  });
+// router.get("/:name", async (req, res) => {
+//     const { name } = req.params;
+//     try {
+//       const restaurant = await Restaurant.findOne(name);
+//       if (!restaurant) {
+//         console.error("An error occured:", error.message);
+//         return res
+//           .status(404)
+//           .json({ error: "The restaurant hasn't been found" });
+//       }
+//       res.status(200).json(restaurant);
+//     } catch (error) {
+//       console.error("An error occured:", error.message);
+//       return res.status(200).json({ error: "The restaurant hasn't been found" });
+//     }
+//   });
 // Other CRUD operations...
 
 module.exports = router;

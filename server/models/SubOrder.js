a5e4ac0ec4806aa52b110eae8465fc4ae693fa1a
DraKkarW2 <68158328+DraKkarW2@users.noreply.github.com>
2024-06-17 16:51:53 +0200

    [fix] order menu add

--- server/models/SubOrder.js
+++ server/models/SubOrder.js
@@ -7,9 +7,18 @@ const subOrderSchema = new Schema({
         articleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Article', required: true },
         quantity: { type: Number, required: true }
     }],
+    Menus: [{
+        menuId: { type: mongoose.Schema.Types.ObjectId, ref: 'Menu', required: true },
+        quantityMenu: { type: Number, required: true },
+        Articles: [{
+            articleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Article', required: true },
+            quantity: { type: Number, required: true }
+        }]
+    }],
     OrderPrice: { type: Number, required: true },
     OrderStatus: { type: String, required: true}
 });
+
 const SubOrder = mongoose.model('SubOrder', subOrderSchema);
 
 module.exports = SubOrder;
